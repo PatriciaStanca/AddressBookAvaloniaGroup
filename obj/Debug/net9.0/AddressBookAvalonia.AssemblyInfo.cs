ea893e7ab2f0caa1e6c3dd624442187f8cf67616
Patricia Stanca <patriciastanca@hotmail.com>
2025-09-29 22:39:29 +0200

    UI update: changed text styles and theme

--- obj/Debug/net9.0/AddressBookAvalonia.AssemblyInfo.cs
+++ obj/Debug/net9.0/AddressBookAvalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookAvalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5828a96fd2870cfba0e41f8dc82ee2eedce878c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b736dd5988381c3a69a2034dbf4c10f1baa139")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookAvalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookAvalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
